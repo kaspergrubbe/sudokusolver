00d8cf2fd1a2636dd63129a186f950ec81bbe07b
Kasper Grubbe <kawsper@gmail.com>
2015-01-08 20:34:47 +0000

    Rename solver in specs from @s to @solver

--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -2,7 +2,7 @@ require 'sudokusolver'
 
 RSpec.describe 'Driver spec' do
   before(:all) do
-    @s = Sudokusolver.new
+    @solver = Sudokusolver.new
 
     @easy  = File.read(File.absolute_path("spec/boards/easy50.txt")).split("\n")
     @top95 = File.read(File.absolute_path("spec/boards/top95.txt")).split("\n")
@@ -10,30 +10,30 @@ RSpec.describe 'Driver spec' do
   end
 
   def multiple(puzzles)
-    puzzles.map{|puzzle| @s.check_solution(@s.string_solution(@s.search(@s.parse_grid(puzzle)))) }
+    puzzles.map{|puzzle| @solver.check_solution(@solver.string_solution(@solver.search(@solver.parse_grid(puzzle)))) }
   end
 
   it 'should give the developers of this gem the idea that it solves properly' do
     puts "Easy puzzle (constraint satisfaction only): "
     puts
-    @s.print_grid(@s.search(@s.parse_grid(@easy.first)))
+    @solver.print_grid(@solver.search(@solver.parse_grid(@easy.first)))
     puts "Hard puzzle (constraint satisfaction + search): "
     puts
-    @s.print_grid(@s.search(@s.parse_grid(@hard.first)))
+    @solver.print_grid(@solver.search(@solver.parse_grid(@hard.first)))
   end
 
   it 'should solve the board' do
     puzzle   = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-    solution = @s.string_solution(@s.search(@s.parse_grid(puzzle)))
+    solution = @solver.string_solution(@solver.search(@solver.parse_grid(puzzle)))
 
-    expect(@s.check_solution(solution)).to be true
+    expect(@solver.check_solution(solution)).to be true
   end
 
   it 'run with an empty puzzle' do
     puzzle = '.................................................................................'
-    solution = @s.string_solution(@s.search(@s.parse_grid(puzzle)))
+    solution = @solver.string_solution(@solver.search(@solver.parse_grid(puzzle)))
 
-    expect(@s.check_solution(solution)).to be true
+    expect(@solver.check_solution(solution)).to be true
   end
 
   it 'should solve all easy boards' do
